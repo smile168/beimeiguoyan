b433c703d29ee1dc27f658dcd35ff40cc4bffe42
smile168 <jasonli110168@gmail.com>
2024-04-07 12:11:39 -0400

    use api for testing

--- src/app/mobile/page.js
+++ src/app/mobile/page.js
@@ -1,11 +1,11 @@
-// async function getCigarette() {
-//   const res = await fetch(`${process.env.SERVER}/api`, { cache: 'no-store' });
-//   return res.json();
-// }
+async function getCigarette() {
+  const res = await fetch(`${process.env.SERVER}/api`, { cache: 'no-store' });
+  return res.json();
+}
 
 export default async function MobileHome() {
-  //   const data = await getCigarette();
-  //   const id = data.id;
-  //   console.log(id);
-  return <p>mobile home, with mongo</p>;
+  const data = await getCigarette();
+  const id = data.id;
+  console.log(id);
+  return <p>mobile home, with mongo {id}</p>;
 }
