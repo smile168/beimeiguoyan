0de26f2a69ebffcc0f72a77c95b9d4cceea77da9
smile168 <jasonli110168@gmail.com>
2024-04-07 22:36:41 -0400

    uninstall mongodb

--- src/app/lib/Cigarettes.js
+++ src/app/lib/Cigarettes.js
@@ -1,12 +1,12 @@
-import { MongoClient } from 'mongodb';
+// import { MongoClient } from 'mongodb';
 
 export async function getCigarettes() {
   // const { searchParams } = new URL(req.url);
   // const id = searchParams.get('id');
-  const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k20f2xq.mongodb.net/`;
-  const client = new MongoClient(url);
-  await client.connect();
-  const db = client.db('Beimeiguoyan');
-  const collection = db.collection('Cigarettes');
-  return await collection.findOne({ id: 'abc' });
+  // const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k20f2xq.mongodb.net/`;
+  // const client = new MongoClient(url);
+  // await client.connect();
+  // const db = client.db('Beimeiguoyan');
+  // const collection = db.collection('Cigarettes');
+  // return await collection.findOne({ id: 'abc' });
 }
