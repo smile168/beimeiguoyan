5f074afa942ffc59bbc1c442b79ac63f33d9ec28
smile168 <jasonli110168@gmail.com>
2024-04-07 22:59:18 -0400

    take out mobile page

--- src/app/mobile/page.js
+++ src/app/mobile/page.js
@@ -1,7 +1,8 @@
 import { getCigarettes } from '../lib/Cigarettes';
 
 export default async function MobileHome() {
-  const data = await getCigarettes();
-  const id = data.id;
-  return <p>mobile home, with mongo {id} restart</p>;
+  // const data = await getCigarettes();
+  // const id = data.id;
+  // return <p>mobile home, with mongo {id} restart</p>;
+  return <p>hellp from mobile page</p>;
 }
