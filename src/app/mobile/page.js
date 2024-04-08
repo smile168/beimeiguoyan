6a9f1b483c84098176480eb8846e9de567c805f1
smile168 <jasonli110168@gmail.com>
2024-04-07 23:23:08 -0400

    take back  mongodb usage

--- src/app/mobile/page.js
+++ src/app/mobile/page.js
@@ -1,8 +1,8 @@
-// import { getCigarettes } from '../lib/Cigarettes';
+import { getCigarettes } from '../lib/Cigarettes';
 
 export default async function MobileHome() {
-  // const data = await getCigarettes();
-  // const id = data.id;
-  // return <p>mobile home, with mongo {id} restart</p>;
+  const data = await getCigarettes();
+  const id = data.id;
+  return <p>mobile home, with mongo {id} restart</p>;
   return <p>hello from mobile home</p>;
 }
