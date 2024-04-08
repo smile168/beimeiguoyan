f95dde4321816ba289c04ce779f959339297aa24
smile168 <jasonli110168@gmail.com>
2024-04-07 23:01:34 -0400

    take mobile mongo call back

--- src/app/mobile/page.js
+++ src/app/mobile/page.js
@@ -1,8 +1,7 @@
 import { getCigarettes } from '../lib/Cigarettes';
 
 export default async function MobileHome() {
-  // const data = await getCigarettes();
-  // const id = data.id;
-  // return <p>mobile home, with mongo {id} restart</p>;
-  return <p>hellp from mobile page</p>;
+  const data = await getCigarettes();
+  const id = data.id;
+  return <p>mobile home, with mongo {id} restart</p>;
 }
