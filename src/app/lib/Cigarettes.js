8c2ac57081953465213124cacf61a5d110873740
smile168 <jasonli110168@gmail.com>
2024-04-07 23:22:10 -0400

    take back mongo reference

--- src/app/lib/Cigarettes.js
+++ src/app/lib/Cigarettes.js
@@ -1,10 +1,10 @@
-// import { MongoClient } from 'mongodb';
+import { MongoClient } from 'mongodb';
 
-// export async function getCigarettes() {
-//   const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k20f2xq.mongodb.net/`;
-//   const client = new MongoClient(url);
-//   await client.connect();
-//   const db = client.db('Beimeiguoyan');
-//   const collection = db.collection('Cigarettes');
-//   return await collection.findOne({ id: 'abc' });
-// }
+export async function getCigarettes() {
+  const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k20f2xq.mongodb.net/`;
+  const client = new MongoClient(url);
+  await client.connect();
+  const db = client.db('Beimeiguoyan');
+  const collection = db.collection('Cigarettes');
+  return await collection.findOne({ id: 'abc' });
+}
