731d99279f27a814ecd06100d5955a164c9259ea
smile168 <jasonli110168@gmail.com>
2024-04-07 22:50:02 -0400

    take out client component

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,16 @@
-'use client';
-import { useEffect, useState } from 'react';
-import MobileHome from '../app/mobile/page';
+// 'use client';
+// import { useEffect, useState } from 'react';
+// import MobileHome from '../app/mobile/page';
 
 export default function Home(props) {
-  const [isMobile, setIsMobile] = useState(false);
+  // const [isMobile, setIsMobile] = useState(false);
 
-  useEffect(() => {
-    const userAgent = window.navigator.userAgent;
-    const mobileRegex =
-      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
-    setIsMobile(mobileRegex.test(userAgent));
-  });
-  return <>{!isMobile ? <p>Desktop Home Page</p> : <MobileHome />}</>;
+  // useEffect(() => {
+  //   const userAgent = window.navigator.userAgent;
+  //   const mobileRegex =
+  //     /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+  //   setIsMobile(mobileRegex.test(userAgent));
+  // });
+  // return <>{!isMobile ? <p>Desktop Home Page</p> : <MobileHome />}</>;
+  return <p>hello from home page</p>;
 }
